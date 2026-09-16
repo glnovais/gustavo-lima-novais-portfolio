b6fdec2d6317d4a91c44c7ff91c9f0d1ac774295
Gustavo Lima Novais <glnovais.ti@gmail.com>
2026-09-16 18:30:45 -0300

    Remove avisos futuros do React Router

--- gustavo-lima-novais-portfolio-humanizado/src/main.tsx
+++ gustavo-lima-novais-portfolio-humanizado/src/main.tsx
@@ -7,7 +7,7 @@ import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <I18nProvider>
         <App />
       </I18nProvider>
